b7e5dcf7a0dfcb30c3f523c73a3920f8d6dcbf8e
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-22 20:17:40 -0300

    Create insertMongoDB.py

---
+++ insertMongoDB.py
@@ -0,0 +1,309 @@
+db.livro.insertMany([
+  {
+    "titulo": "O Sol é Para Todos",
+    "autor": "Harper Lee",
+    "ano": 1960,
+    "isbn": "978-0061120084"
+  },
+  {
+    "titulo": "1984",
+    "autor": "George Orwell",
+    "ano": 1949,
+    "isbn": "978-0451524935"
+  },
+  {
+    "titulo": "Dom Casmurro",
+    "autor": "Machado de Assis",
+    "ano": 1899,
+    "isbn": "978-8572324943"
+  }]),
+  {
+    "titulo": "O Pequeno Príncipe",
+    "autor": "Antoine de Saint-Exupéry",
+    "ano": 1943,
+    "isbn": "978-8595080808"
+  },
+  {
+    "titulo": "Cem Anos de Solidão",
+    "autor": "Gabriel García Márquez",
+    "ano": 1967,
+    "isbn": "978-0060883287"
+  },
+  {
+    "titulo": "Moby Dick",
+    "autor": "Herman Melville",
+    "ano": 1851,
+    "isbn": "978-1503280786"
+  },
+  {
+    "titulo": "Orgulho e Preconceito",
+    "autor": "Jane Austen",
+    "ano": 1813,
+    "isbn": "978-1503290563"
+  },
+  {
+    "titulo": "O Grande Gatsby",
+    "autor": "F. Scott Fitzgerald",
+    "ano": 1925,
+    "isbn": "978-0743273565"
+  },
+  {
+    "titulo": "A Revolução dos Bichos",
+    "autor": "George Orwell",
+    "ano": 1945,
+    "isbn": "978-0451526342"
+  },
+  {
+    "titulo": "O Senhor dos Anéis",
+    "autor": "J.R.R. Tolkien",
+    "ano": 1954,
+    "isbn": "978-0544003415"
+  },
+  {
+    "titulo": "O Apanhador no Campo de Centeio",
+    "autor": "J.D. Salinger",
+    "ano": 1951,
+    "isbn": "978-0316769488"
+  },
+  {
+    "titulo": "A Montanha Mágica",
+    "autor": "Thomas Mann",
+    "ano": 1924,
+    "isbn": "978-1472521421"
+  },
+  {
+    "titulo": "O Conde de Monte Cristo",
+    "autor": "Alexandre Dumas",
+    "ano": 1844,
+    "isbn": "978-0140449266"
+  },
+  {
+    "titulo": "Crime e Castigo",
+    "autor": "Fiódor Dostoiévski",
+    "ano": 1866,
+    "isbn": "978-0486415871"
+  },
+  {
+    "titulo": "Ulisses",
+    "autor": "James Joyce",
+    "ano": 1922,
+    "isbn": "978-0141182803"
+  },
+  {
+    "titulo": "As Aventuras de Sherlock Holmes",
+    "autor": "Arthur Conan Doyle",
+    "ano": 1892,
+    "isbn": "978-0553212419"
+  },
+  {
+    "titulo": "O Processo",
+    "autor": "Franz Kafka",
+    "ano": 1925,
+    "isbn": "978-0805210408"
+  },
+  {
+    "titulo": "A Divina Comédia",
+    "autor": "Dante Alighieri",
+    "ano": 1320,
+    "isbn": "978-0142437223"
+  },
+  {
+    "titulo": "O Morro dos Ventos Uivantes",
+    "autor": "Emily Brontë",
+    "ano": 1847,
+    "isbn": "978-0141439556"
+  },
+  {
+    "titulo": "Drácula",
+    "autor": "Bram Stoker",
+    "ano": 1897,
+    "isbn": "978-0141439846"
+  },
+  {
+    "titulo": "Alice no País das Maravilhas",
+    "autor": "Lewis Carroll",
+    "ano": 1865,
+    "isbn": "978-1503250215"
+  },
+  {
+    "titulo": "O Livro do Desassossego",
+    "autor": "Fernando Pessoa",
+    "ano": 1982,
+    "isbn": "978-9723718087"
+  },
+  {
+    "titulo": "Memórias Póstumas de Brás Cubas",
+    "autor": "Machado de Assis",
+    "ano": 1881,
+    "isbn": "978-8572325124"
+  },
+  {
+    "titulo": "Os Miseráveis",
+    "autor": "Victor Hugo",
+    "ano": 1862,
+    "isbn": "978-0192834235"
+  },
+  {
+    "titulo": "O Velho e o Mar",
+    "autor": "Ernest Hemingway",
+    "ano": 1952,
+    "isbn": "978-0684801223"
+  },
+  {
+    "titulo": "O Estrangeiro",
+    "autor": "Albert Camus",
+    "ano": 1942,
+    "isbn": "978-0679720201"
+  },
+  {
+    "titulo": "Hamlet",
+    "autor": "William Shakespeare",
+    "ano": 1603,
+    "isbn": "978-0743477123"
+  },
+  {
+    "titulo": "A Metamorfose",
+    "autor": "Franz Kafka",
+    "ano": 1915,
+    "isbn": "978-0141188126"
+  },
+  {
+    "titulo": "Don Quixote",
+    "autor": "Miguel de Cervantes",
+    "ano": 1605,
+    "isbn": "978-0060934347"
+  },
+  {
+    "titulo": "O Hobbit",
+    "autor": "J.R.R. Tolkien",
+    "ano": 1937,
+    "isbn": "978-0547928227"
+  },
+  {
+    "titulo": "Senhor das Moscas",
+    "autor": "William Golding",
+    "ano": 1954,
+    "isbn": "978-0399501487"
+  },
+  {
+    "titulo": "O Retrato de Dorian Gray",
+    "autor": "Oscar Wilde",
+    "ano": 1890,
+    "isbn": "978-0141442464"
+  },
+  {
+    "titulo": "Em Busca do Tempo Perdido",
+    "autor": "Marcel Proust",
+    "ano": 1913,
+    "isbn": "978-0375751547"
+  },
+  {
+    "titulo": "Guerra e Paz",
+    "autor": "Liev Tolstói",
+    "ano": 1869,
+    "isbn": "978-1400079988"
+  },
+  {
+    "titulo": "Jane Eyre",
+    "autor": "Charlotte Brontë",
+    "ano": 1847,
+    "isbn": "978-0141441146"
+  },
+  {
+    "titulo": "O Nome da Rosa",
+    "autor": "Umberto Eco",
+    "ano": 1980,
+    "isbn": "978-0156001311"
+  },
+  {
+    "titulo": "O Iluminado",
+    "autor": "Stephen King",
+    "ano": 1977,
+    "isbn": "978-0307743657"
+  },
+  {
+    "titulo": "A Sangue Frio",
+    "autor": "Truman Capote",
+    "ano": 1966,
+    "isbn": "978-0679745587"
+  },
+  {
+    "titulo": "Os Irmãos Karamazov",
+    "autor": "Fiódor Dostoiévski",
+    "ano": 1880,
+    "isbn": "978-0374528378"
+  },
+  {
+    "titulo": "Admirável Mundo Novo",
+    "autor": "Aldous Huxley",
+    "ano": 1932,
+    "isbn": "978-0060850524"
+  },
+  {
+    "titulo": "O Perfume",
+    "autor": "Patrick Süskind",
+    "ano": 1985,
+    "isbn": "978-0375725844"
+  },
+  {
+    "titulo": "O Lobo da Estepe",
+    "autor": "Hermann Hesse",
+    "ano": 1927,
+    "isbn": "978-0142437186"
+  },
+  {
+    "titulo": "O Código Da Vinci",
+    "autor": "Dan Brown",
+    "ano": 2003,
+    "isbn": "978-0307474278"
+  },
+  {
+    "titulo": "O Vento nos Salgueiros",
+    "autor": "Kenneth Grahame",
+    "ano": 1908,
+    "isbn": "978-0451530141"
+  },
+  {
+    "titulo": "Frankenstein",
+    "autor": "Mary Shelley",
+    "ano": 1818,
+    "isbn": "978-0486282114"
+  },
+  {
+    "titulo": "Coração das Trevas",
+    "autor": "Joseph Conrad",
+    "ano": 1899,
+    "isbn": "978-0486264646"
+  },
+  {
+    "titulo": "Os Contos de Canterbury",
+    "autor": "Geoffrey Chaucer",
+    "ano": 1400,
+    "isbn": "978-0140424389"
+  },
+  {
+    "titulo": "O Leopardo",
+    "autor": "Giuseppe Tomasi di Lampedusa",
+    "ano": 1958,
+    "isbn": "978-0375714794"
+  },
+  {
+    "titulo": "Madame Bovary",
+    "autor": "Gustave Flaubert",
+    "ano": 1857,
+    "isbn": "978-0140449129"
+  },
+  {
+    "titulo": "A Cor Púrpura",
+    "autor": "Alice Walker",
+    "ano": 1982,
+    "isbn": "978-0156028356"
+  },
+  {
+    "titulo": "O Idiota",
+    "autor": "Fiódor Dostoiévski",
+    "ano": 1869,
+    "isbn": "978-0375702241"
+  }
+]
+)
